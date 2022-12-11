58c059db3fa653a3e42fec0daffe3c43ccfb83c2
michalgealdor224 <nhnh224@gmail.com>
2022-12-11 21:11:09 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,199 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Board from "./board";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Circle() {
+   return (
+       <div>
+           <Board/>
+       </div>
+   );
 }
 
-export default App;
+function changeColor() {
+    return (
+
+        <div height="110" width="500">
+            <circle2
+                cx="50"
+                cy="55"
+                r="45"
+                fill="none"
+                stroke="#F0CE01"
+                strokeWidth="4"
+            />
+
+        </div>);
+
+}
+class App extends React.Component {
+    state= { circles : [{locations: "11", color:"white"},21,31,41,51,61,71,12,22,32,42,52,62,72]
+
+    }
+
+
+
+ render() {
+     return (
+
+         <table>
+       <tr>
+           <td> </td>
+           {
+           Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td></td>
+           {
+               Circle()
+           }
+
+       </tr>
+<tr>
+</tr>
+       <tr>
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td></td>
+           {
+               Circle()
+           }
+       </tr>
+       <tr>
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td></td>
+           {
+               Circle()
+           }
+       </tr>
+       <tr>
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td></td>
+           {
+               Circle()
+           }
+       </tr>
+       <tr>
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td> </td>
+           {
+               Circle()
+           }
+           <td></td>
+           {
+               Circle()
+           }
+       </tr>
+      </table>)
+
+ }
+
+}
+export default Circle;
---
+++ src/Board.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Board() {
+    return (
+        <div height="110" width="500">
+            <circle1
+                cx="50"
+                cy="55"
+                r="45"
+                fill="none"
+                stroke="#F0CE01"
+                strokeWidth="4"
+            />
+
+
+        </div>
+
+    );
+
+}export default Board
